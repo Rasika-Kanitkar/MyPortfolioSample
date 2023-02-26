156a194a61081f251f2398a2fda17203f2df028c
Rasika Kanitkar <k22rasika@gmail.com>
2023-02-26 19:03:32 +0530

    Portfolio Added

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+ 
+ 
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+ 
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+ 
+// Dark Mode
+let darkmode = document.querySelector('#darkmode');
+ 
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('active');
+    }else{
+        darkmode.classList.replace('bx-sun','bx-moon');
+        document.body.classList.remove('active');
+    }
+}
+
+function sendEmail() {
+    Email.send({
+        Host : "sandbox.smtp.mailtrap.io",
+        Username : "c51cb0ace719ef",
+        Password : "449a10dae249dd",
+        To : 'k22rasika@gmail.com',
+        From : "sender@example.com",
+        Subject : "Test email",
+        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(
+      message => alert(message)
+    );
+    }
